33e6f67276400b320de6707bef3e53f3e7a0d108
Erik Johnston <erikj@jki.re>
2015-07-11 23:45:27 +0100

    Add more benchmarks

--- benches/lib.rs
+++ benches/lib.rs
@@ -39,6 +39,36 @@ Hello
 
 "#;
 
+const TEST_REQUEST_GITHUB_GET : &'static [u8] =
+br#"GET /joyent/http-parser/ HTTP/1.1
+Host: github.com
+Connection: keep-alive
+Cache-Control: max-age=0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.9 Safari/537.36
+Referer: https://github.com/joyent/http-parser
+Accept-Encoding: gzip, deflate, sdch
+Accept-Language: en-GB,en-US;q=0.8,en;q=0.6,nb;q=0.4
+
+"#;
+
+// Shamelessly taken from: https://github.com/joyent/http-parser/blob/master/bench.c
+const TEST_REQUEST_JOYENT_HTTP_PARSER_BENCH : &'static [u8] =
+b"POST /joyent/http-parser HTTP/1.1\r\n\
+Host: github.com\r\n\
+DNT: 1\r\n\
+Accept-Encoding: gzip, deflate, sdch\r\n\
+Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4\r\n\
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) \
+    AppleWebKit/537.36 (KHTML, like Gecko) \
+    Chrome/39.0.2171.65 Safari/537.36\r\n\
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,\
+    image/webp,*/*;q=0.8\r\n\
+Referer: https://github.com/joyent/http-parser\r\n\
+Connection: keep-alive\r\n\
+Transfer-Encoding: chunked\r\n\
+Cache-Control: max-age=0\r\n\r\nb\r\nhello world\r\n0\r\n\r\n";
+
 
 macro_rules! benchmark_strings {
     ($($name:ident: $s:expr),+$(,)*) => {
@@ -58,7 +88,9 @@ macro_rules! benchmark_strings {
 benchmark_strings! {
     bench_length: TEST_REQUEST_LENGTH,
     bench_chunked: TEST_REQUEST_CHUNKED,
-    bench_chunked_params: TEST_REQUEST_CHUNKED_PARAMS
+    bench_chunked_params: TEST_REQUEST_CHUNKED_PARAMS,
+    bench_github_get: TEST_REQUEST_GITHUB_GET,
+    joyent_http_parser: TEST_REQUEST_JOYENT_HTTP_PARSER_BENCH,
 }
 
 
